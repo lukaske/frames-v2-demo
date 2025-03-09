6486e9085539d7327c821e5bd4dafb99a9c3d983
Luka Skeledzija <36110818+lukaske@users.noreply.github.com>
2025-03-09 13:21:22 +0100

    dploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    // Disable TypeScript errors during build
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Disable ESLint during builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
